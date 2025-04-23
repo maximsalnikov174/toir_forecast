a4117f4722c1e608f4ad078ec991277ba68efb1a
Maxim Salnikov <maximsalnikov174@icloud.com>
2025-04-23 16:04:19 +0500

    рефакторинг схем еще

---
+++ toir_app/schemas/special_status.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from pydantic import Field
+
+from toir_app.models.static_model import SpecialStatusForCar
+from toir_app.schemas.mixins import TimestampMixin
+from toir_app.schemas.user import UserBase
+
+
+class SpecialStatusWithTimestamp(TimestampMixin):
+    """
+    Модель статуса Автомобиля с меткой времени и информацией о Пользователе.
+
+    Примеры статусов:
+    - к выбытию
+    - на реализации
+    - после ВР
+    """
+    status: Optional[SpecialStatusForCar] = Field(
+        None,
+        title='Текущий статус'
+    )
+    from_user: Optional[UserBase] = Field(
+        None,
+        title='Пользователь, присвоивший статус'
+    )
