f7febaa9c616f2342466bd8ca3002245771c9835
Maxim Salnikov <maximsalnikov174@icloud.com>
2025-05-21 23:38:21 +0500

    upd main (todo)

--- toir_app/main.py
+++ toir_app/main.py
@@ -5,10 +5,12 @@ import uvicorn
 from fastapi import FastAPI
 from dotenv import load_dotenv
 
+from toir_app.core.db import AsyncSessionLocal
 from toir_app.api.endpoints import router
 from toir_app.core.config import settings
 from toir_app.convert_csv_to_py.parse_data import (
-    convert_csv_to_list, upload_filedata_in_db
+    convert_csv_to_list,
+    upload_filedata_in_db
 )
 from toir_app.convert_csv_to_py.upload_data import (
     upload_all_users_data_in_db,
@@ -22,22 +24,30 @@ toir_app = FastAPI(title=settings.app_title)
 # Подключаем роутер к приложению:
 toir_app.include_router(router)
 script_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = f'{script_dir}/dataset_from_oebs/rmt321_full_new_part3.csv'
+file_path = f'{script_dir}/dataset_from_oebs/rmt321_full_new.csv'
 
 
 async def main():
     """Основная асинхронная функция инициализации"""
-    if os.environ['UPLOAD_STATIC_DATA_FROM_CSV'].lower() == 'true':
-        # 1. Загружаем enum-значения в БД
-        await upload_all_users_data_in_db(need_to_upload_datas)
+    async with AsyncSessionLocal() as download_session:
+        if os.environ['UPLOAD_STATIC_DATA_FROM_CSV'].lower() == 'true':
+            # 1. Загружаем enum-значения в БД (по итогу - коммит, он нужен)
+            await upload_all_users_data_in_db(
+                need_to_upload_datas, download_session
+            )
 
-    if os.environ['UPLOAD_DATA_FROM_CSV'].lower() == 'true':
-        # 2. Конвертируем CSV и обновляем БД
-        lst = await convert_csv_to_list(file_path)
-        for element in lst:
-            await upload_filedata_in_db(element)
+        if os.environ['UPLOAD_DATA_FROM_CSV'].lower() == 'true':
+            # 2. Конвертируем CSV и обновляем БД
+            lst = await convert_csv_to_list(file_path)  # нужно ли здесь async?
+
+            # TODO
+            # Следующие 2 строчки - место для БОЛЬШОГО рефакторинга:
+            # Можно (читать-НУЖНО!) проверять, чтобы не было в сессии и в базе
+            for element in lst:
+                await upload_filedata_in_db(element, download_session)
 
     # 3. Запускаем FastAPI сервер
+    # (не понятно, как тут дальше начнет работать сессия)
     config = uvicorn.Config(
         'toir_app.main:toir_app',
         reload=True
@@ -49,7 +59,6 @@ async def main():
 if __name__ == '__main__':
     # Запускаем асинхронный main()
     asyncio.run(main())
-
 
 # При запуске приложения через python main.py, работа с env-файлом строится
 # иначе — нужно указать полный или относительный путь до env-файла либо
