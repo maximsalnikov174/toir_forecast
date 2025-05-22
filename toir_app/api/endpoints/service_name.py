a813729e7917ff4f463fbfc99fb1cc106728c31e
Maxim Salnikov <maximsalnikov174@icloud.com>
2025-05-22 20:09:25 +0500

    endpoint service_name

---
+++ toir_app/api/endpoints/service_name.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from toir_app.core.db import get_async_session
+from toir_app.crud.toir_app_crud import (
+    check_service_status_by_param,
+    get_service_name_with_request_status
+)
+from toir_app.schemas.service_name import ServiceNameBase
+
+
+router = APIRouter()
+
+
+@router.get(
+    '/all_service_names',
+    response_model=list[ServiceNameBase],
+    name='Срез видов ТО',
+    description='Получение среза видов ТО для заполнения шапки таблицы.',
+    status_code=200,
+)
+async def get_all_service_names_with_selected_request_status(
+    request_status_param: int,
+    session: AsyncSession = Depends(get_async_session)
+):
+    """Возвращает список видов ТО с выбранным Присвоенным Статусом."""
+    # Проверяем существование выбранного присвоенного статуса в БД:
+    await check_service_status_by_param(session, request_status_param)
+    return await get_service_name_with_request_status(
+        request_status_param, session
+    )
