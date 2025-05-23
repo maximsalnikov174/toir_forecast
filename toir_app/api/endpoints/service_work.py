c541093571b4ab237dcbbb97c4631c0da1914d6e
Maxim Salnikov <maximsalnikov174@icloud.com>
2025-05-23 20:19:12 +0500

    POST for models.ServiceWork - add zvr to service_work

--- toir_app/api/endpoints/service_work.py
+++ toir_app/api/endpoints/service_work.py
@@ -6,25 +6,28 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from toir_app.core.db import get_async_session
 from toir_app.crud.service_work import check_zvr_unique, get_service_work
-from toir_app.schemas.service_work import ServiceWorkBase
-
+from toir_app.schemas.service_work import (
+    ServiceWorkWithZVRNumber
+)
 
 router = APIRouter()
 
 
 @router.post(
     '/add_zvr',
-    response_model=ServiceWorkBase,
+    response_model=ServiceWorkWithZVRNumber,
     name='Добавление ЗВР к конкретному service_work',
     response_model_exclude_none=True,
     status_code=201
 )
 async def add_zvr_to_service_work(
     service_work_id: int,
-    zvr_number: Annotated[int, Field(ge=1_000_000, le=9_999_999)],
+    zvr_number: Annotated[int, Field(ge=1_000_000, lt=10_000_000)],
     session: AsyncSession = Depends(get_async_session)
 ):
+    """Добавление 7-значного ЗВР к service_work."""
     service_work = await get_service_work(service_work_id, session)
+
     if not service_work:
         raise HTTPException(
             status_code=404,
@@ -38,9 +41,11 @@ async def add_zvr_to_service_work(
     if await check_zvr_unique(zvr_number, session):
         raise HTTPException(
             status_code=422,
-            detail='ЗВР не уникален.'
+            detail=f'Указанный ЗВР #{zvr_number} не уникален, сверьте данные.'
         )
+
     service_work.zvr_number = zvr_number
+
     await session.flush()
     await session.commit()
     return service_work
