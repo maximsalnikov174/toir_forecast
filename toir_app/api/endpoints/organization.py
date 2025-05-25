058c415df560aa849f453dc1d41841986d31f93b
Maxim Salnikov <maximsalnikov174@icloud.com>
2025-05-25 12:06:23 +0500

    endpoint - organization - get by ID

--- toir_app/api/endpoints/organization.py
+++ toir_app/api/endpoints/organization.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from toir_app.core.db import get_async_session
 from toir_app.crud.organization import (
-    get_organization_list
+    get_organization_list,
+    get_current_organization
 )
 from toir_app.schemas.organization import OrganizationResponse
 
@@ -14,13 +15,32 @@ router = APIRouter()
 @router.get(
     '/all',
     response_model=list[OrganizationResponse],
-    name='Список подразделений',
+    name='Получение списка подразделений (на выбор пользователю)',
     description='Получение списка всех подразделений для отбора по цеху.',
     status_code=200,
 )
 async def get_all_organization(
     session: AsyncSession = Depends(get_async_session)
 ):
-    """Возвращает список видов ТО с выбранным Присвоенным Статусом."""
-    # Проверяем существование выбранного присвоенного статуса в БД:
+    """Возвращает список всех подразделений."""
     return await get_organization_list(session)
+
+
+@router.get(
+    '/{organization_id}',
+    response_model=OrganizationResponse,
+    name='Получение информации о выбранном пользователем подразделении',
+    description='Получение подразделения для отбора по цеху.',
+    status_code=200,
+)
+async def get_organization(
+    organization_id: int,
+    session: AsyncSession = Depends(get_async_session)
+):
+    """Возвращает выбранное пользователем подразделение."""
+    organization = await get_current_organization(organization_id, session)
+    if not organization:
+        raise HTTPException(
+            404, detail=f'Подразделение с ID={organization_id} не найдено'
+        )
+    return organization
--- toir_app/crud/organization.py
+++ toir_app/crud/organization.py
@@ -8,7 +8,19 @@ async def get_organization_list(session: AsyncSession) -> list[Organization]:
     """Возврат списка подразделений."""
     organization_list = await session.scalars(
         select(Organization)
-        # .where(ServiceWork.request_status_id == request_status_id)
         .order_by(Organization.name)  # сортировка по Юхх
     )
     return organization_list.all()
+
+
+async def get_current_organization(
+        organization_id: int,
+        session: AsyncSession
+) -> Organization:
+    """Возврат выбранного подразделения."""
+    organization = await session.scalars(
+        select(Organization)
+        .where(Organization.id == organization_id)
+        .order_by(Organization.name)  # сортировка по Юхх
+    )
+    return organization.first()
