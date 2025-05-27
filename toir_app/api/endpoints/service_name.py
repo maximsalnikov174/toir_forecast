002b80f0acb3b0f2354dbae271ce6444ae8e5609
Maxim Salnikov <maximsalnikov174@icloud.com>
2025-05-27 15:35:27 +0500

    ServiceName - endpoint - рефакторинг отбора

--- toir_app/api/endpoints/service_name.py
+++ toir_app/api/endpoints/service_name.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -14,7 +15,8 @@ from toir_app.schemas.service_name import ServiceNameBase
 router = APIRouter()
 
 
-@router.get(
+# TODO ПРОВЕРИТЬ как работает фильтрация на простых примерах!!!
+@router.post(
     '/all_service_names',
     response_model=list[ServiceNameBase],
     name='Срез видов ТО',
@@ -22,12 +24,26 @@ router = APIRouter()
     status_code=200,
 )
 async def get_all_service_names_with_selected_request_status(
+    *,
     request_status_param: int,
+    special_status_ids: list[Optional[int]],
+    organization_id: int,
     session: AsyncSession = Depends(get_async_session)
 ):
-    """Возвращает список видов ТО с выбранным Присвоенным Статусом."""
+    """Возвращает список видов ТО с выбранным Присвоенным Статусом.
+
+    Args:
+        request_status_param (int): id выбранного юзером расчётного статуса.
+        special_status_ids (list(int)): список ids специальных статусов.
+        organization_id (int): id выбранного юзером подразделения.
+        session: сессия.
+    """
     # Проверяем существование выбранного присвоенного статуса в БД:
     await check_service_status_by_param(session, request_status_param)
+
     return await get_service_name_with_request_status(
-        request_status_param, session
+        request_status_id=request_status_param,
+        special_status_ids=special_status_ids,
+        organization_id=organization_id,
+        session=session
     )
--- toir_app/crud/service_name.py
+++ toir_app/crud/service_name.py
@@ -1,22 +1,39 @@
 from typing import Optional
-from sqlalchemy import select
+from sqlalchemy import or_, select
+# from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from toir_app.models import ServiceName, ServiceWork
+from toir_app.models import ServiceName, ServiceWork, Car
 
 
 async def get_service_name_with_request_status(
     request_status_id: int,
+    special_status_ids: list[Optional[int]],
+    organization_id: int,
     session: AsyncSession
 ) -> list[Optional[ServiceName]]:
-    """
-    Возврат УНИКАЛЬНЫХ видов ТО c выбранным Присвоенным статусом.
+    """Возврат УНИКАЛЬНЫХ видов сервисного обслуживания.
+
+    Учитываются выбранные пользователем:
+    - расчётный статус;
+    - список специальных статусов ТС (FIXME без статуса -> всегда)
+    - подразделение.
     """
     service_names = await session.execute(
         select(ServiceName)
         .join(ServiceWork, ServiceName.id == ServiceWork.next_service_id)
-        .where(ServiceWork.request_status_id == request_status_id)
-        .distinct()  # distinct - дедупликация
+        .join(Car)
+        # .options(
+        #     joinedload(ServiceWork.car),
+        # )
+        .where(
+            ServiceWork.request_status_id == request_status_id,
+            Car.organization_id == organization_id,
+            or_(
+                Car.special_status_id.in_(special_status_ids),
+                Car.special_status_id.is_(None)
+            )
+        ).distinct()  # distinct - дедупликация
         .order_by(ServiceName.id)  # сортировка по ID вида работ
     )
     return list(service_names.scalars().all())
