f7cab913db213daa59113c8f3cac5898eb81ccda
Maxim Salnikov <maximsalnikov174@icloud.com>
2025-05-29 09:44:44 +0500

    ServiceName - CRUD - расширение метода получения уникальных видов ТО в зависимости от статусов

--- toir_app/crud/service_name.py
+++ toir_app/crud/service_name.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from sqlalchemy import or_, select
+from sqlalchemy import and_, or_, select
 # from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -10,14 +10,26 @@ async def get_service_name_with_request_status(
     request_status_id: int,
     special_status_ids: list[Optional[int]],
     organization_id: int,
-    session: AsyncSession
+    session: AsyncSession,
+    need_range: bool = False
 ) -> list[Optional[ServiceName]]:
     """Возврат УНИКАЛЬНЫХ видов сервисного обслуживания.
 
-    Учитываются выбранные пользователем:
-    - расчётный статус;
-    - список специальных статусов ТС (FIXME без статуса -> всегда)
-    - подразделение.
+    Args:
+        - need_range - чтобы указать, что нужен диапазон.
+
+    Users filters:
+        - расчётный статус.
+        - список специальных статусов ТС (FIXME без статуса -> всегда).
+        - подразделение.
+
+    Default filters:
+        - запись о сервисном обслуживании не в архиве.
+        - ТС не в архиве.
+        - дедупликация объектов ServiceName (вроде).
+
+    Order by:
+        - asc IDs ServiceName.
     """
     service_names = await session.execute(
         select(ServiceName)
@@ -27,11 +39,20 @@ async def get_service_name_with_request_status(
         #     joinedload(ServiceWork.car),
         # )
         .where(
-            ServiceWork.request_status_id == request_status_id,
-            Car.organization_id == organization_id,
-            or_(
-                Car.special_status_id.in_(special_status_ids),
-                Car.special_status_id.is_(None)
+            and_(
+                ServiceWork.in_archive.is_(False),
+                (
+                    # выбираем точное == или диапазон до цели и все что строже
+                    ServiceWork.request_status_id <= request_status_id
+                    if need_range
+                    else ServiceWork.request_status_id == request_status_id
+                ),
+                Car.organization_id == organization_id,
+                Car.in_archive.is_(False),
+                or_(
+                    Car.special_status_id.in_(special_status_ids),
+                    Car.special_status_id.is_(None)
+                )
             )
         ).distinct()  # distinct - дедупликация
         .order_by(ServiceName.id)  # сортировка по ID вида работ
