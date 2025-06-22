36025704f081133dfdedca185bd62ac640146362
Maxim Salnikov <maximsalnikov174@icloud.com>
2025-06-22 22:13:44 +0500

    add imports

--- toir_app/core/base.py
+++ toir_app/core/base.py
@@ -1,4 +1,6 @@
 # Импортируем базовый класс Base.
 from toir_app.core.db import Base  # noqa
 from toir_app.models import (Car, CarModel, Organization, ServiceName,  # noqa
-                             ServiceStatus, ServiceWork, SpecialStatus, User)
+                             ServiceStatus, ServiceStatusStats,  # noqa
+                             ServiceWork, ServiceWorkState,  # noqa
+                             SpecialStatus, Status, Role, User)  # noqa
--- toir_app/models/__init__.py
+++ toir_app/models/__init__.py
@@ -1,12 +1,13 @@
-from .car_model import CarModel  # noqa
+from .static_model import Status, SpecialStatusForCar, UserRole  # noqa
 from .car import Car  # noqa
+from .car_model import CarModel  # noqa
 from .organization import Organization  # noqa
 from .role import Role  # noqa
 from .service_name import ServiceName  # noqa
 from .service_status import ServiceStatus  # noqa
 from .service_work import ServiceWork  # noqa
 from .special_status import SpecialStatus  # noqa
-from .static_model import Status, SpecialStatusForCar, UserRole  # noqa
 from .station import Station  # noqa
+from .stats import ServiceWorkState, ServiceStatusStats  # noqa
 from .user import User  # noqa
 from .role import Role  # noqa
