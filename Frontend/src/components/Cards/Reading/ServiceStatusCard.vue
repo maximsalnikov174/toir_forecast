95e343e197e3b2e63165662d98c1e33b28636df7
protopaer <protopaer@gmail.com>
2025-07-25 12:38:56 +0500

    +

--- Frontend/src/components/Cards/Reading/ServiceStatusCard.vue
+++ Frontend/src/components/Cards/Reading/ServiceStatusCard.vue
@@ -198,7 +198,7 @@ const showBottomBar = computed(() => {
   position: absolute;
   width: 120px;
   height: 22px;
-  top: 30px;
+  top: 29px;
   left: 22px;
   font-family: 'Inter', sans-serif;
   font-weight: 700;
