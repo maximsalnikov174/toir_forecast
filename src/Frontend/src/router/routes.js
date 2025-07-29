e64be905147deaa7371020699aae5b92687b0715
Maxim Salnikov <maximsalnikov174@icloud.com>
2025-07-29 19:41:10 +0500

    upd frontend

--- src/Frontend/src/router/routes.js
+++ src/Frontend/src/router/routes.js
@@ -4,7 +4,6 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'), // исправлено на MainLayout
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'test-list', component: () => import('src/components/TestList.vue') } // рекомендуем использовать kebab-case для URL
     ]
   },
   {
