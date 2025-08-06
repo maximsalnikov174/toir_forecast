f6d1adfef0fcc6b930a23fa35a4fb1d1bca6f128
protopaer <protopaer@gmail.com>
2025-08-06 12:07:03 +0500

    добавил select

--- src/Frontend/src/components/Cards/AddCarSpecialStatus.vue
+++ src/Frontend/src/components/Cards/AddCarSpecialStatus.vue
@@ -1,7 +1,21 @@
 <template>
   <q-card style="min-width: 300px">
     <q-card-section>
-      <div class="text-h6">Тест</div>
+      <div class="text-h6">Выберите статус</div>
+
+      <q-select
+        v-model="selectedSpecialStatuses"
+        :options="statusOptions"
+        option-label="name"
+        option-value="id"
+        label="Статус"
+        filled
+        map-options
+        emit-value
+        :loading="loading"
+        multiple
+        clearable
+      />
     </q-card-section>
 
     <q-card-actions align="right">
@@ -12,12 +26,23 @@
 
 <script>
 import { defineComponent } from 'vue'
+import { useStatusOptions } from '../Functions/ButtonSelectGroupTs.js' // Или правильный путь к вашему файлу
 
 export default defineComponent({
   name: 'AddCarSpecialStatus',
   emits: ['close'],
   setup() {
-    return {}
+    const {
+      statusOptions,
+      selectedSpecialStatuses,
+      loading
+    } = useStatusOptions()
+
+    return {
+      statusOptions,
+      selectedSpecialStatuses,
+      loading
+    }
   }
 })
 </script>
