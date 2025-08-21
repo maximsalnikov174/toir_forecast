174f1d0213e21be7e825b7fa940f81e592fdd982
Maxim Salnikov <maximsalnikov174@icloud.com>
2025-08-21 10:25:28 +0500

    fix imports

--- src/toir_app/core/user.py
+++ src/toir_app/core/user.py
@@ -4,7 +4,6 @@ from typing import Optional, Union
 from fastapi import Depends, Request
 from fastapi_users import (
     BaseUserManager,
-    exceptions,
     FastAPIUsers,
     IntegerIDMixin,
     InvalidPasswordException,
