273ce7288cc3b782a1819c919111d3d06df43ba7
protopaer <protopaer@gmail.com>
2025-09-08 09:04:08 +0500

    +

--- src/Frontend/src/boot/axios.js
+++ src/Frontend/src/boot/axios.js
@@ -8,8 +8,8 @@ import axios from "axios";
 // "export default () => {}" function below (which runs individually
 // for each client)
 
-//const api = axios.create({ baseURL: 'http://127.0.0.1:8001' });
-const api = axios.create({ baseURL: 'http://toir.atu.mmk.ru:8877' });
+const api = axios.create({ baseURL: 'http://127.0.0.1:8001' });
+//const api = axios.create({ baseURL: 'http://toir.atu.mmk.ru:8877' });
 
 
 const token = localStorage.getItem("token");
